8520d5a970f16d0fa47cbbf5fddf9599e6287247
Setu Shah <setu@Setus-MacBook-Pro.local>
2018-09-10 09:34:27 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>I'm Setu Shah, a proactive student enrolled fulltime in Master's of Computer Science at Arizona State University. Coding is my passion and hence I would love to explore freeCodeCamp hackathon and yield greater experience and knowledge in my field of expertise. There is a lot more about me, that amazes me and would amaze you too! Let's catch up at the hackathon ;)</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
